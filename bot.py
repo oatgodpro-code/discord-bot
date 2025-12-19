08da5d6b96de731b8a1a2b7bc848b1cc74abf14f
oatgodpro-code <oatgodpro@gmail.com>
2025-12-19 22:10:28 +0700

    bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong!")
+
+bot.run(os.environ["TOKEN"])
